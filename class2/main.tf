8491a4cc9c10fae8f5b46ff209d598b816534897
amandakaly03 <amandakaly03@gmail.com>
2024-07-06 16:51:59 +0000

    check

--- class2/main.tf
+++ class2/main.tf
@@ -2,24 +2,24 @@ provider aws {
     region = "us-east-2"
 }
 
-resource "aws_key_pair" "deployer" {
-  key_name   = "deployer-key"
-  public_key = file("~/.ssh/id_rsa.pub")
-}
+# resource "aws_key_pair" "deployer" {
+#   key_name   = "deployer-key"
+#   public_key = file("~/.ssh/id_rsa.pub")
+# }
 
-resource "aws_s3_bucket" "example" {
-  bucket = "kaizen-amandatest"
- // force_destroy = true
-}
+# resource "aws_s3_bucket" "example" {
+#   bucket = "kaizen-amandatest"
+#  // force_destroy = true
+# }
 
-resource "aws_s3_object" "object" {
-depends_on = [ ]
-  bucket = "kaizen-amandatest"
-  key    = "main.tf"
-  source = "main.tf"
-}
+# resource "aws_s3_object" "object" {
+# depends_on = [ ]
+#   bucket = "kaizen-amandatest"
+#   key    = "main.tf"
+#   source = "main.tf"
+# }
 
-resource "aws_s3_bucket" "example" {
-  bucket = "kaizenfr-"
-  force_destroy = true
-}
+# resource "aws_s3_bucket" "example" {
+#   bucket = "kaizenfr-"
+#   force_destroy = true
+# }
